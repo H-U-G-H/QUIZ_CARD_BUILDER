96b4f046d3ed43d1976037646a83d83b91002932
Исмаилов Саид <mazgOFF.net@mail.ru>
2022-09-26 13:35:50 +0300

    feat: прокоментирован класс QuizCard

--- quiz/QuizCard.java
+++ quiz/QuizCard.java
@@ -5,17 +5,17 @@ public class QuizCard
     private String question = null;
     private String answer = null;
 
-    public QuizCard(String question, String answer)
+    public QuizCard(String question, String answer) // йнмярпсйрнп
     {
         this.question = question;
         this.answer = answer;
     }
 
-    public String getQuestion()
+    public String getQuestion() // опнярни церреп дкъ бнопняю
     {
         return question;
     }
-    public String getAnswer()
+    public String getAnswer() // опнярни церреп дкъ нрберю
     {
         return answer;
     }
